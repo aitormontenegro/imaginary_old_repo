268d2b57ac4832a6eaf4923451b08482d32d262a
Aitor <aitor@bodas.net>
2018-11-05 18:14:03 +0100

    new commit

--- vendor/gopkg.in/h2non/bimg.v1/vips.h
+++ vendor/gopkg.in/h2non/bimg.v1/vips.h
@@ -521,8 +521,8 @@ vips_watermark_image(VipsImage *in, VipsImage *sub, VipsImage **out, WatermarkIm
 		vips_extract_band(t[1], &t[3], t[1]->Bands - 1, "n", 1, NULL) ||
 		vips_linear1(t[3], &t[4], o->Opacity, 0.0, NULL) ||
 		vips_cast(t[4], &t[5], VIPS_FORMAT_UCHAR, NULL) ||
-//		vips_copy(t[5], &t[6], "interpretation", t[0]->Type, NULL) ||
-		vips_embed(t[5], &t[7], o->Left, o->Top, t[0]->Xsize, t[0]->Ysize, NULL))	{
+		vips_copy(t[5], &t[6], "interpretation", t[0]->Type, NULL) ||
+		vips_embed(t[6], &t[7], o->Left, o->Top, t[0]->Xsize, t[0]->Ysize, NULL))	{
 			g_object_unref(base);
 		return 1;
 	}
