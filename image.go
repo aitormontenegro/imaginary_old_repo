b329f1a8d1356cd0b2bb3029bb193ceec0086aba
Aitor <aitor@bodas.net>
2018-11-07 10:03:17 +0100

    new-commit

--- image.go
+++ image.go
@@ -145,6 +145,7 @@ func AddWatermarkImage (o ImageOptions, buf2 []byte, opts bimg.Options)(Image, e
         }
         o.Image = o.CustomWatermark;
 
+
         fmt.Printf("--> %f\n",o.WatermarkOpacity);
         /*
         if o.WatermarkOpacity != 0 {
@@ -153,6 +154,8 @@ func AddWatermarkImage (o ImageOptions, buf2 []byte, opts bimg.Options)(Image, e
             o.Opacity = 1.2
         }
         */
+
+        o.Opacity = 1.1
 
         return watermarkImage(swapimage.Body, o)
     }else{
