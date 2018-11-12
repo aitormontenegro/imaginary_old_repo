961e61269b5a77224ae7bd72eb685b2b9219f164
Aitor <aitor@bodas.net>
2018-11-12 12:18:13 +0100

    Try saving cache with channels

--- vendor/gopkg.in/h2non/bimg.v1/resize.go
+++ vendor/gopkg.in/h2non/bimg.v1/resize.go
@@ -2,17 +2,18 @@
 
 package bimg
 
-/*
 import (
 	"runtime"
 )
-*/
 
 // Resize is used to transform a given image as byte buffer
 // with the passed options.
 func Resize(buf []byte, o Options) ([]byte, error) {
 	// Required in order to prevent premature garbage collection. See:
 	// https://github.com/h2non/bimg/pull/162
-//	defer runtime.KeepAlive(buf)
+	defer runtime.KeepAlive(buf)
+
+    fmt.Printf("A. size = %d \n", len(buf))
+
 	return resizer(buf, o)
 }
