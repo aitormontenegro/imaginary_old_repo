f9bd7771f1268c79d2fd51b2f5b430894bcfec61
Aitor <aitor@bodas.net>
2018-11-12 12:36:36 +0100

    Try saving cache with channels

--- vendor/gopkg.in/h2non/bimg.v1/resizer.go
+++ vendor/gopkg.in/h2non/bimg.v1/resizer.go
@@ -174,7 +174,7 @@ func saveImage(image *C.VipsImage, o Options) ([]byte, error) {
 		Lossless:       o.Lossless,
 	}
 	// Finally get the resultant buffer
-    fmt.Printf("E. size = %d \n", len(image))
+    fmt.Printf("E. size = %+v \n", len(vipsSave(image, saveOptions)))
 	return vipsSave(image, saveOptions)
 }
 
